ca6421e5612a9c963203416aa4edcdc11bc5ce86
MatheusBrandalise <matheusjosebrandalise@gmail.com>
2022-01-06 11:06:03 -0300

    Terceiro exercício finalizado

---
+++ src/devInHouseSeniorFirstWeek/fullName.java
@@ -0,0 +1,20 @@
+package devInHouseSeniorFirstWeek;
+
+import java.util.Scanner;
+
+public class fullName {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Qual o seu sobrenome?");
+		String lastName = sc.next();
+		
+		System.out.println("Qual o seu nome?");
+		String firstName = sc.next();
+
+		System.out.println("Seu nome completo é: " + firstName + " " + lastName);
+		
+	}
+
+}
